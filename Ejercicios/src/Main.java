d0b97828f182903ff46ce4dc46e2411dc7ef324c
licjosechavez <t10_chavez@hotmail.com>
2022-05-18 10:54:09 -0300

    Ejercicio 5 - 1 al 10

--- Ejercicios/src/Main.java
+++ Ejercicios/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /* Encabezado*/
+
+        System.out.println("Sentecias condicionales \n");
+        System.out.println("Ejercicio 5 \n");
+
+        //Resolución
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Nº: " + i);
+        }
+
     }
 }
