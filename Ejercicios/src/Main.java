421ee3014e0f7c7980bd962c6122d3f130e4b0ce
licjosechavez <t10_chavez@hotmail.com>
2022-05-18 10:47:07 -0300

    Ejercicio 4 - foo

--- Ejercicios/src/Main.java
+++ Ejercicios/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /* Encabezado*/
+
+        System.out.println("Sentecias condicionales\n");
+        System.out.println("Ejercicio 4\n");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("foo");
+        }
+
+
     }
 }
