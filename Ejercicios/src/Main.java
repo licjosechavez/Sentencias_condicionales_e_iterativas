1c507916dc6da434d28487e126d1bdf22e931610
licjosechavez <t10_chavez@hotmail.com>
2022-05-18 10:37:34 -0300

    Ejercicio 3 - Colegio

--- Ejercicios/src/Main.java
+++ Ejercicios/src/Main.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Variables
+
+        Scanner scanner = new Scanner(System.in);
+
+        /* Encabezado*/
+
+        System.out.println("Sentecias condicionales \n");
+        System.out.println("Ejercicio 3 \n");
+
+        //Tomar datos
+
+        System.out.println("Ingrese el nombre del cursante: ");
+        String nombreCursante = scanner.nextLine();
+        System.out.println("Ingrese la nota Nº 1 (entre 1 y 10): ");
+        int nota1 = scanner.nextInt();
+        System.out.println("Ingrese la nota Nº 2 (entre 1 y 10): ");
+        int nota2 = scanner.nextInt();
+        System.out.println("Ingrese la nota Nº 3 (entre 1 y 10): ");
+        int nota3 = scanner.nextInt();
+
+        // Cálculos
+
+        float promedioNotas = (nota1 + nota2 + nota3) / 3;
+
+        // Evaluación
+
+        String estado = "";
+        if(promedioNotas >= 1 && promedioNotas < 6){
+            estado = "Desaprobado";
+        }else if(promedioNotas >= 6 && promedioNotas < 9){
+            estado = "Aprobado";
+        }else if(promedioNotas >= 9 && promedioNotas <= 10){
+            estado = "Promocionado";
+        }
+
+        //Salida
+
+        System.out.println("===================================="
+                + " Colegio Universitario: "
+                + "====================================\n\n"
+                + " Nombre del cursante: " + nombreCursante + "\n"
+                + " Nota 1: " + nota1  + "\n"
+                + " Nota 2: " + nota2  + "\n"
+                + " Nota 3: " + nota3  + "\n"
+                + " Promedio: " + promedioNotas  + "\n"
+                + " Estado: " + estado  + "\n"
+        );
     }
 }
