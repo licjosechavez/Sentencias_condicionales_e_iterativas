0fe3383b460323b4f7a9bb0e6a2fd07e8b607d7e
licjosechavez <t10_chavez@hotmail.com>
2022-05-18 10:01:33 -0300

    Ejercicio 2

--- Ejercicios/src/Main.java
+++ Ejercicios/src/Main.java
@@ -1,29 +1,89 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        //Variables
+
+        boolean flag = true;
+        Scanner scanner = new Scanner(System.in);
+
         /* Encabezado*/
 
         System.out.println("Sentecias condicionales \n");
-        System.out.println("Ejercicio 1 \n");
+        System.out.println("Ejercicio 2 \n");
 
-        /* Captura de datos*/
+        //Tomar datos
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese el primer número: ");
-        int numero1 = scanner.nextInt();
-        System.out.println("Ingrese el segundo número: ");
-        int numero2 = scanner.nextInt();
+        System.out.println("Ingrese el nombre del producto: ");
+        String nombreProducto = scanner.nextLine();
+        System.out.println("Ingrese el precio del producto: ");
+        float precioProducto = scanner.nextFloat();
+        DecimalFormat df = new DecimalFormat("0.00");
 
-        /*Evaluar numero mayor*/
+        do{
 
-        if(numero1 == numero2){
-            System.out.println("Los números son iguales");
-        }else if(numero1 > numero2){
-            System.out.println("El número mayor es: " + numero1);
-        }else {
-            System.out.println("El número mayor es: " + numero2);
-        }
+            System.out.println("\nSeleccione un método de pago: -->\n"
+                    + "\t\t\t1. Contado (15% de descuento)\n"
+                    + "\t\t\t2. Débito (mismo precio)\n"
+                    + "\t\t\t3. Tarjeta (10% de recargo)\n"
+                    + "\t\t\t0. Cancelar compra\n");
+
+            int metodoPago = scanner.nextInt();
+
+            switch(metodoPago){
+                case 1:{
+
+                    float contado = precioProducto - ((precioProducto * 15) / 100);
+                    System.out.println("===================================="
+                            + " Precio de contado: "
+                            + "====================================\n\n"
+                            + " Nombre del producto: " + nombreProducto + "\n"
+                            + " Precio final: $" + contado
+                    );
+                    flag = false;
+                    break;
+                }
+
+                case 2:{
+                    System.out.println("===================================="
+                            + " Precio con débito: "
+                            + "====================================\n\n"
+                            + " Nombre del producto: " + nombreProducto + "\n"
+                            + " Precio final: $" + precioProducto
+                    );
+                    flag = false;
+                    break;
+                }
+
+                case 3:{
+                    float tarjeta = precioProducto + ((precioProducto * 10) / 100);
+                    System.out.println("===================================="
+                            + " Precio con tarjeta: "
+                            + "====================================\n\n"
+                            + " Nombre del producto: " + nombreProducto + "\n"
+                            + " Precio final: $" + tarjeta
+                    );
+                    flag = false;
+                    break;
+                }
+
+                case 0:{
+                    //Salida del sistema
+                    System.out.println("===================================="
+                            +" La compra ha sido cancelada! "
+                            +"====================================\n");
+                    flag = false;
+                    break;
+                }
+
+                default:{
+                    System.out.println("La opción seleccionada es incorrecta.\n Ingrese un valor correcto.");
+                    break;
+                }
+            }
+        }while(flag!=false);
+
     }
 }
