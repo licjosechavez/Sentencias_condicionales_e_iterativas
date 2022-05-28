5db4cdbcc7f6b1f4db0f2854669fb7f96503e902
licjosechavez <t10_chavez@hotmail.com>
2022-05-28 11:00:07 -0300

    Pasado a metodo

--- Ejercicios/src/Main.java
+++ Ejercicios/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        foo();
+
+
+    }
+
+    private static void foo() {
         /* Encabezado*/
 
         System.out.println("Sentecias condicionales\n");
@@ -9,7 +15,5 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println("foo");
         }
-
-
     }
 }
