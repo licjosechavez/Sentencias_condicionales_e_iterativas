24404de4c9308066e8152e1387a022eb7e6475b4
licjosechavez <t10_chavez@hotmail.com>
2022-05-28 18:04:45 -0300

    Subida de Ejercicios con menú

--- Ejercicios/src/Main.java
+++ Ejercicios/src/Main.java
@@ -4,16 +4,126 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-        importeCompra();
-
-    }
-
-    private static void importeCompra() {
         //Variables
 
         boolean flag = true;
-        
+        Scanner scanner = new Scanner(System.in);
+
+        /* Encabezado*/
+
+        System.out.println("EJERCICIOS: Sentecias condicionales y GIT \n");
+        System.out.println("MENU \n");
+
+        /* Menú y métodos*/
+
+        do{
+
+            System.out.println("====================================" +
+                    "\nSeleccione un ejercicio para mostrar: -->\n"
+                    + "\t\t\t1. Número Mayor\n"
+                    + "\t\t\t2. Importe de la compra\n"
+                    + "\t\t\t3. Colegio\n"
+                    + "\t\t\t4. Foo\n"
+                    + "\t\t\t5. Número 1 al 10\n"
+                    + "\t\t\t6. Triágulos\n"
+                    + "\t\t\t0. Salir del programa\n");
+
+            int optionMenu = scanner.nextInt();
+
+            switch(optionMenu){
+                case 1:{
+
+                    System.out.println("===================================="
+                            + " Ejercicio 1: "
+                            + "====================================\n\n"
+                            + " Número mayor"
+                    );
+                    numeroMayor();
+                    flag = true;
+                    break;
+                }
+
+                case 2:{
+                    System.out.println("===================================="
+                            + " Ejercicio 2: "
+                            + "====================================\n\n"
+                            + " Importe de la compra"
+
+                    );
+                    importeCompra();
+                    flag = true;
+                    break;
+                }
+
+                case 3:{
+
+                    System.out.println("===================================="
+                            + " Ejercicio 3: "
+                            + "====================================\n\n"
+                            + " Colegio"
+                    );
+                    notasColegio();
+                    flag = true;
+                    break;
+                }
+
+                case 4:{
+
+                    System.out.println("===================================="
+                            + " Ejercicio 4: "
+                            + "====================================\n\n"
+                            + " Foo"
+                    );
+                    foo();
+                    flag = true;
+                    break;
+                }
+
+                case 5:{
+
+                    System.out.println("===================================="
+                            + " Ejercicio 5: "
+                            + "====================================\n\n"
+                            + " Números 1 al 10"
+                    );
+                    nros_1_al_10();
+                    flag = true;
+                    break;
+                }
+
+                case 6:{
+
+                    System.out.println("===================================="
+                            + " Ejercicio 6: "
+                            + "====================================\n\n"
+                            + " Triángulos"
+                    );
+                    triangulos();
+                    flag = true;
+                    break;
+                }
+
+                case 0:{
+                    //Salida del sistema
+                    System.out.println("===================================="
+                            +" Ha salido del programa!. Hasta pronto! "
+                            +"====================================\n");
+                    flag = false;
+                    break;
+                }
+
+                default:{
+                    System.out.println("La opción seleccionada es incorrecta.\n Ingrese un valor correcto.");
+                    break;
+                }
+            }
+        }while(flag!=false);
+
+
+    }
+
+    private static void numeroMayor() {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese el primer número: ");
         int numero1 = scanner.nextInt();
@@ -32,195 +142,14 @@ public class Main {
 
 
 
-        triangulos();
-
-
     }
 
-    private static void triangulos() {
-        /* Encabezado*/
+    private static void importeCompra() {
 
-        System.out.println("Sentecias condicionales \n");
-        System.out.println("Ejercicio 6 \n");
-
-        //Resolucion a
-        /*
-
-        *
-        * *
-        * * *
-        * * * *
-        * * * * *
-        * * * * * *
-        * * * * * * *
-
-        */
-
-        //Resolucion b
-
-        /*
-
-         * * * * * * *
-         * * * * * *
-         * * * * *
-         * * * *
-         * * *
-         * *
-         *
-
-         */
-
-        //Resolucion c
-
-        /*
-
-                   *
-                 * *
-               * * *
-             * * * *
-           * * * * *
-         * * * * * *
-       * * * * * * *
-
-         */
-
-        System.out.println("\nTriangulo a\n");
-
-        for (int i = 0; i < 7; i++){
-            String asterisco = "";
-
-            for(int j = 0; j < i + 1; j++){
-                asterisco += "*";
-            }
-            System.out.println(asterisco);
-
-        }
-
-
-        //Resolucion b
-        System.out.println("\nTriangulo b\n");
-
-        for (int i = 0; i < 7; i++){
-            String asterisco = "";
-
-            for(int j = 0; j < 7 - i; j++){
-                asterisco += "*";
-            }
-            System.out.println(asterisco);
-
-        }
-
-        //Resolucion c
-        System.out.println("\nTriangulo c\n");
-
-        for (int i = 0; i < 7; i++){
-            String asterisco = "";
-
-            for(int j = 0; j < 6 - i; j++){
-                asterisco += " ";
-            }
-
-            for (int j = 0; j < i + 1; j++) {
-                asterisco += "*";
-            }
-            System.out.println(asterisco);
-
-        }
-
-
-        nros_1_al_10();
-
-
-    }
-
-    private static void nros_1_al_10() {
-        /* Encabezado*/
-
-        System.out.println("Sentecias condicionales \n");
-        System.out.println("Ejercicio 5 \n");
-
-        //Resolución
-
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("Nº: " + i);
-        }
-
-
-        foo();
-
-
-
-    }
-
-    private static void foo() {
-        /* Encabezado*/
-
-        System.out.println("Sentecias condicionales\n");
-        System.out.println("Ejercicio 4\n");
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("foo");
-        }
-
-
-        notasColegio();
-
-    }
-
-    private static void notasColegio() {
         //Variables
 
+        boolean flag = true;
         Scanner scanner = new Scanner(System.in);
-
-        /* Encabezado*/
-
-        System.out.println("Sentecias condicionales \n");
-        System.out.println("Ejercicio 3 \n");
-
-        //Tomar datos
-
-        System.out.println("Ingrese el nombre del cursante: ");
-        String nombreCursante = scanner.nextLine();
-        System.out.println("Ingrese la nota Nº 1 (entre 1 y 10): ");
-        int nota1 = scanner.nextInt();
-        System.out.println("Ingrese la nota Nº 2 (entre 1 y 10): ");
-        int nota2 = scanner.nextInt();
-        System.out.println("Ingrese la nota Nº 3 (entre 1 y 10): ");
-        int nota3 = scanner.nextInt();
-
-        // Cálculos
-
-        float promedioNotas = (nota1 + nota2 + nota3) / 3;
-
-        // Evaluación
-
-        String estado = "";
-        if(promedioNotas >= 1 && promedioNotas < 6){
-            estado = "Desaprobado";
-        }else if(promedioNotas >= 6 && promedioNotas < 9){
-            estado = "Aprobado";
-        }else if(promedioNotas >= 9 && promedioNotas <= 10){
-            estado = "Promocionado";
-        }
-
-        //Salida
-
-        System.out.println("===================================="
-                + " Colegio Universitario: "
-                + "====================================\n\n"
-                + " Nombre del cursante: " + nombreCursante + "\n"
-                + " Nota 1: " + nota1  + "\n"
-                + " Nota 2: " + nota2  + "\n"
-                + " Nota 3: " + nota3  + "\n"
-                + " Promedio: " + promedioNotas  + "\n"
-                + " Estado: " + estado  + "\n"
-        );
-
-        numeroMayor();
-
-    }
-
-    private static void numeroMayor() {
 
         /* Encabezado*/
 
@@ -298,8 +227,190 @@ public class Main {
                 }
             }
         }while(flag!=false);
-
         
+
+
+
+
+
+
+
+    }
+
+    private static void notasColegio() {
+        //Variables
+
+        Scanner scanner = new Scanner(System.in);
+
+        /* Encabezado*/
+
+        System.out.println("Sentecias condicionales \n");
+        System.out.println("Ejercicio 3 \n");
+
+        //Tomar datos
+
+        System.out.println("Ingrese el nombre del cursante: ");
+        String nombreCursante = scanner.nextLine();
+        System.out.println("Ingrese la nota Nº 1 (entre 1 y 10): ");
+        int nota1 = scanner.nextInt();
+        System.out.println("Ingrese la nota Nº 2 (entre 1 y 10): ");
+        int nota2 = scanner.nextInt();
+        System.out.println("Ingrese la nota Nº 3 (entre 1 y 10): ");
+        int nota3 = scanner.nextInt();
+
+        // Cálculos
+
+        float promedioNotas = (nota1 + nota2 + nota3) / 3;
+
+        // Evaluación
+
+        String estado = "";
+        if(promedioNotas >= 1 && promedioNotas < 6){
+            estado = "Desaprobado";
+        }else if(promedioNotas >= 6 && promedioNotas < 9){
+            estado = "Aprobado";
+        }else if(promedioNotas >= 9 && promedioNotas <= 10){
+            estado = "Promocionado";
+        }
+
+        //Salida
+
+        System.out.println("===================================="
+                + " Colegio Universitario: "
+                + "====================================\n\n"
+                + " Nombre del cursante: " + nombreCursante + "\n"
+                + " Nota 1: " + nota1  + "\n"
+                + " Nota 2: " + nota2  + "\n"
+                + " Nota 3: " + nota3  + "\n"
+                + " Promedio: " + promedioNotas  + "\n"
+                + " Estado: " + estado  + "\n"
+        );
+
+
+    }
+
+    private static void foo() {
+        /* Encabezado*/
+
+        System.out.println("Sentecias condicionales\n");
+        System.out.println("Ejercicio 4\n");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("foo");
+        }
+
+
+
+
+    }
+    private static void nros_1_al_10() {
+        /* Encabezado*/
+
+        System.out.println("Sentecias condicionales \n");
+        System.out.println("Ejercicio 5 \n");
+
+        //Resolución
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Nº: " + i);
+        }
+
+
+
+
+
+
+    }
+
+    private static void triangulos() {
+        /* Encabezado*/
+
+        System.out.println("Sentecias condicionales \n");
+        System.out.println("Ejercicio 6 \n");
+
+        //Resolucion a
+        /*
+
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * * * *
+         * * * * * * *
+
+         */
+
+        //Resolucion b
+
+        /*
+
+         * * * * * * *
+         * * * * * *
+         * * * * *
+         * * * *
+         * * *
+         * *
+         *
+
+         */
+
+        //Resolucion c
+
+        /*
+
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * * * *
+         * * * * * * *
+
+         */
+
+        System.out.println("\nTriangulo a\n");
+
+        for (int i = 0; i < 7; i++){
+            String asterisco = "";
+
+            for(int j = 0; j < i + 1; j++){
+                asterisco += "*";
+            }
+            System.out.println(asterisco);
+
+        }
+
+
+        //Resolucion b
+        System.out.println("\nTriangulo b\n");
+
+        for (int i = 0; i < 7; i++){
+            String asterisco = "";
+
+            for(int j = 0; j < 7 - i; j++){
+                asterisco += "*";
+            }
+            System.out.println(asterisco);
+
+        }
+
+        //Resolucion c
+        System.out.println("\nTriangulo c\n");
+
+        for (int i = 0; i < 7; i++){
+            String asterisco = "";
+
+            for(int j = 0; j < 6 - i; j++){
+                asterisco += " ";
+            }
+
+            for (int j = 0; j < i + 1; j++) {
+                asterisco += "*";
+            }
+            System.out.println(asterisco);
+
+        }
 
 
     }
