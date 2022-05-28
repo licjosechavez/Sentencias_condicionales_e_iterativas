6adb26935e1484af7e89a057550dce5cfd70c532
licjosechavez <t10_chavez@hotmail.com>
2022-05-28 10:02:44 -0300

    Pasado a metodo

--- Ejercicios/src/Main.java
+++ Ejercicios/src/Main.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        numeroMayor();
+
+    }
+
+    private static void numeroMayor() {
         /* Encabezado*/
 
         System.out.println("Sentecias condicionales \n");
